a877cd41bb618a5705ec324ecc4d57ce13eae5c8
Nivrutti07 <nivruttipatil8618@gmail.com>
2026-01-05 18:22:49 +0530

    Update customer.sql

--- sql_insert_values_code/customer.sql
+++ sql_insert_values_code/customer.sql
@@ -1,1 +1,11 @@
+INSERT INTO customer VALUES (1,'Prince','K','Jain','prince.jain@gmail.com',DATE '2003-04-12',9876543210,0);
+INSERT INTO customer VALUES (2,'Kedar','A','Nimbalkar','kedar.nimbalkar@gmail.com',DATE '2002-08-21',9765432109,0);
+INSERT INTO customer VALUES (3,'Ramesh','S','Patil','ramesh.patil@gmail.com',DATE '2001-01-15',9123456789,0);
+INSERT INTO customer VALUES (4,'Anvit','P','Naik','anvit.naik@gmail.com',DATE '2004-06-18',9988776655,0);
+INSERT INTO customer VALUES (5,'Saurabh','R','Deshpande','saurabh.deshpande@gmail.com',DATE '2003-11-09',9898989898,0);
+INSERT INTO customer VALUES (6,'Aditya','M','Kulkarni','aditya.kulkarni@gmail.com',DATE '2002-05-27',9012345678,0);
+INSERT INTO customer VALUES (7,'Rohit','N','Joshi','rohit.joshi@gmail.com',DATE '2001-09-03',9321456780,0);
+INSERT INTO customer VALUES (8,'Akshay','V','Kamble','akshay.kamble@gmail.com',DATE '2003-03-30',9845612378,0);
+INSERT INTO customer VALUES (9,'Shubham','A','Chavan','shubham.chavan@gmail.com',DATE '2004-12-02',9090909090,0);
+INSERT INTO customer VALUES (10,'Omkar','S','Hegde','omkar.hegde@gmail.com',DATE '2002-07-14',9445566778,0);
 
