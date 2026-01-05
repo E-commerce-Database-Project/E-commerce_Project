a274aca0f8ec0b1be2f6a8f1ce21abfd4e2cb5fe
Nivrutti07 <nivruttipatil8618@gmail.com>
2026-01-05 18:12:59 +0530

    Update Payment_Table.sql

--- Database Creation/Payment_Table.sql
+++ Database Creation/Payment_Table.sql
@@ -1,8 +1,13 @@
-create table e_commerce.payment(
-paymentMode enum('online','offline'),
-dateofpayment datetime,
-order_id int,
-foreign key (order_id) references order_table(order_id) on delete set null on update no action,
-customer_id int,
-FOREIGN KEY (customer_id) REFERENCES customer(customer_id) on delete set null on update no action
-);	
+CREATE TABLE payment (
+  paymentMode VARCHAR2(20)
+    CHECK (paymentMode IN ('online','offline')),
+  dateofpayment TIMESTAMP,
+  order_id NUMBER,
+  customer_id NUMBER,
+  FOREIGN KEY (order_id)
+    REFERENCES order_table(order_id)
+    ON DELETE SET NULL,
+  FOREIGN KEY (customer_id)
+    REFERENCES customer(customer_id)
+    ON DELETE SET NULL
+);
