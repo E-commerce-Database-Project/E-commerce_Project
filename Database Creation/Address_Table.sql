64fcdce1a6cd2282ef6bca1c5326841e3dae69ca
Nivrutti07 <nivruttipatil8618@gmail.com>
2026-01-05 18:09:55 +0530

    Update Address_Table.sql

--- Database Creation/Address_Table.sql
+++ Database Creation/Address_Table.sql
@@ -1,11 +1,13 @@
-create table e_commerce.address(
-address_id int primary key,
-apart_no int(10),
-apart_name varchar(255),
-streetname varchar(255),
-state varchar(255),
-city varchar(255),
-pincode int(6),
-customer_id int ,
-FOREIGN KEY (customer_id) REFERENCES customer(customer_id) on delete set null on update no action
+CREATE TABLE address (
+  address_id NUMBER PRIMARY KEY,
+  apart_no VARCHAR2(10),
+  apart_name VARCHAR2(255),
+  streetname VARCHAR2(255),
+  state VARCHAR2(255),
+  city VARCHAR2(255),
+  pincode NUMBER(6),
+  customer_id NUMBER,
+  FOREIGN KEY (customer_id)
+    REFERENCES customer(customer_id)
+    ON DELETE CASCADE
 );
