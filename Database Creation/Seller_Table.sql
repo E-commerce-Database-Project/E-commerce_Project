beebc3acaf69ba29f8b43c58662cfddea66674f2
Nivrutti07 <nivruttipatil8618@gmail.com>
2026-01-05 18:08:16 +0530

    Update Seller_Table.sql

--- Database Creation/Seller_Table.sql
+++ Database Creation/Seller_Table.sql
@@ -1,9 +1,6 @@
-create table e_commerce.review(
-review_id int primary key,
-description varchar(255),
-rating enum('1','2','3','4','5'),
-customer_id int,
-FOREIGN KEY (customer_id) REFERENCES customer(customer_id) on delete set null on update no action,
-product_id int,
-FOREIGN KEY (product_id) REFERENCES product(product_id) on delete set null on update no action
+CREATE TABLE seller (
+  seller_id NUMBER PRIMARY KEY,
+  seller_name VARCHAR2(255),
+  seller_phone NUMBER(15),
+  total_sales NUMBER(10,2)
 );
