df59ca21b8d9806fc86ef0dbaf5ef3525d8a79e3
Nivrutti07 <nivruttipatil8618@gmail.com>
2026-01-05 18:07:08 +0530

    Update Customer_Table.sql

--- Database Creation/Customer_Table.sql
+++ Database Creation/Customer_Table.sql
@@ -1,13 +1,10 @@
-create schema e_commerce;
-
-create table e_commerce.customer (
-customer_id int primary key,
-FirstName varchar(50),
-MiddleName varchar(50),
-LastName varchar(50),
-Email varchar(100),
-DateOfBirth date,
-phone INT(10),
-age int null
+CREATE TABLE customer (
+  customer_id NUMBER PRIMARY KEY,
+  FirstName VARCHAR2(50),
+  MiddleName VARCHAR2(50),
+  LastName VARCHAR2(50),
+  Email VARCHAR2(100),
+  DateOfBirth DATE,
+  phone NUMBER(15),
+  age NUMBER
 );
-
