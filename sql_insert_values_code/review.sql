3247b887dd85d6c57bd3f8be921ab973d3003cb0
Nivrutti07 <nivruttipatil8618@gmail.com>
2026-01-05 18:49:39 +0530

    Update review.sql

--- sql_insert_values_code/review.sql
+++ sql_insert_values_code/review.sql
@@ -1,1 +1,11 @@
+INSERT INTO review VALUES (1,'Good phone',5,1,1);
+INSERT INTO review VALUES (2,'Laptop performance good',4,2,2);
+INSERT INTO review VALUES (3,'Mouse is smooth',4,3,3);
+INSERT INTO review VALUES (4,'Shirt quality nice',3,4,4);
+INSERT INTO review VALUES (5,'Kurti is comfortable',5,5,5);
+INSERT INTO review VALUES (6,'Shoes are durable',4,6,6);
+INSERT INTO review VALUES (7,'Mixer works well',5,7,7);
+INSERT INTO review VALUES (8,'Book very helpful',4,8,8);
+INSERT INTO review VALUES (9,'Watch battery good',4,9,9);
+INSERT INTO review VALUES (10,'Gaming mouse excellent',5,10,10);
 
