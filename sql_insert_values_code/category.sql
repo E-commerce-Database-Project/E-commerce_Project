99cd0f1583bb50e117155c6b0719a1d6ade5e7b2
Nivrutti07 <nivruttipatil8618@gmail.com>
2026-01-05 18:23:07 +0530

    Update category.sql

--- sql_insert_values_code/category.sql
+++ sql_insert_values_code/category.sql
@@ -1,1 +1,11 @@
+INSERT INTO category VALUES (1,'Mobiles','smartphones');
+INSERT INTO category VALUES (2,'Laptops','personal laptops');
+INSERT INTO category VALUES (3,'Accessories','mouse, keyboard');
+INSERT INTO category VALUES (4,'Men Fashion','clothing');
+INSERT INTO category VALUES (5,'Women Fashion','apparel');
+INSERT INTO category VALUES (6,'Footwear','shoes');
+INSERT INTO category VALUES (7,'Home Appliances','electronics');
+INSERT INTO category VALUES (8,'Books','education');
+INSERT INTO category VALUES (9,'Watches','smart watches');
+INSERT INTO category VALUES (10,'Gaming','gaming accessories');
 
