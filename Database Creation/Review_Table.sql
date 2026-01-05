17a2b42b712a3e9bda535e44ccb8e7bd14bfc956
Nivrutti07 <nivruttipatil8618@gmail.com>
2026-01-05 18:11:53 +0530

    Update Review_Table.sql

--- Database Creation/Review_Table.sql
+++ Database Creation/Review_Table.sql
@@ -1,9 +1,13 @@
-create table e_commerce.review(
-review_id int primary key,
-description varchar(255),
-rating enum('1','2','3','4','5'),
-customer_id int,
-FOREIGN KEY (customer_id) REFERENCES customer(customer_id) on delete set null on update no action,
-product_id int,
-FOREIGN KEY (product_id) REFERENCES product(product_id) on delete set null on update no action
+CREATE TABLE review (
+  review_id NUMBER PRIMARY KEY,
+  description VARCHAR2(255),
+  rating NUMBER(1) CHECK (rating BETWEEN 1 AND 5),
+  customer_id NUMBER,
+  product_id NUMBER,
+  FOREIGN KEY (customer_id)
+    REFERENCES customer(customer_id)
+    ON DELETE SET NULL,
+  FOREIGN KEY (product_id)
+    REFERENCES product(product_id)
+    ON DELETE SET NULL
 );
